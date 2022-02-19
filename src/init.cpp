a05f671b7c7e4fee6071f6f4ab6ec37333a55b0b
Ariston Lorenzo <4tl0renz0@gmail.com>
2022-02-19 15:31:06 -0500

    init: Save repo name and description to .neon/config.txt
    Signed-off-by: Ariston Lorenzo <4tl0renz0@gmail.com>

--- src/init.cpp
+++ src/init.cpp
@@ -1,13 +1,32 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <iostream>
+#include <string>
 
-int init_repository() 
+int main() 
 {
 	// Check if folder .neon exists
 	// If not, create it
 	if (mkdir(".neon", 0777) == -1)
 		printf("Error initalizing repository\n");
-	else
+	else {
 		printf("Initialized repository\n");
+
+		// Get user input for repository name and description
+		printf("Enter repository name: ");
+		char name[100];
+		scanf("%s", name);
+		// TODO : Currently the description does not print anything after one space.
+		// If some one would like to figure out how to print the full input (with spaces)
+		// then please do so. - 4tl
+		printf("Enter repository description: ");
+		std::string description[100];
+		scanf("%s", description);
+
+		// Create .neon/config.txt using name and description
+		FILE *fp = fopen(".neon/config.txt", "w");
+		fprintf(fp, "Repository Name: %s\nRepository Descripton: %s", name, description);
+		fclose(fp);
+	}
+
 }
